bea285e389f2ac716aaa4abd70ba74589325d1e4
Hugo Freire <hfreire@exec.sh>
2019-06-23 11:27:39 +0200

    fix: tweak output names

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "aws_vpc" {
-  value = data.aws_vpc.selected
+output "aws_vpc_id" {
+  value = data.aws_vpc.selected.id
 }
 
 output "aws_subnet_ids" {
-  value = data.aws_subnet_ids.selected
+  value = data.aws_subnet_ids.selected.ids
 }
 
-output "aws_security_groups" {
-  value = data.aws_security_groups.selected
+output "aws_security_group_ids" {
+  value = data.aws_security_groups.selected.ids
 }
